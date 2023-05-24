f14dda1b4686edd9326adf6b005ba01757be83db
Arash Kardanpour <arash@gmail.com>
2023-05-23 21:23:31 -0700

    #Main commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arash KP</title>
+</head>
+<body>
+<h1 class="text-2xl font-bold">
+                Install Tailwind CSS 3 In Laravel 10
+                <br>
+                <span class="text-red-600">
+                    <h2 class="text-3xl">Arashkp.com</h2>
+                </span>
+            </h1>
+</body>
+</html>
